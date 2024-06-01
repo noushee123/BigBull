90a333442cf6e87827309f63385b04526656cfb4
Siddharthmarko <Siddhartha.marko.3@gmail.com>
2024-06-01 12:18:55 +0530

    Setup React router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { useState } from 'react'
 import './App.css'
+import Home from './component/home';
+import {Routes, Route} from 'react-router-dom';
+
 
 function App() {
-  const [count, setCount] = useState(0)
   
   return (
     <>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+    </Routes>
      <h1 className='text-3xl font-bold underline'>Hello</h1>
     </>
   )
